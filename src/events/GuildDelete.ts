a046d41c4ae960f04980aa8edb3f20d23da11bc8
Théo <43091603+glazk0@users.noreply.github.com>
2023-08-30 17:26:43 +0200

    fix: docker

--- src/events/GuildDelete.ts
+++ src/events/GuildDelete.ts
@@ -1,6 +1,9 @@
 import { Events, Guild } from 'discord.js';
 
 import { Event } from '../structures/Event';
+import { db } from '../db';
+import { guilds, news } from '../db/schema';
+import { eq } from 'drizzle-orm';
 
 export default class GuildDelete extends Event {
   constructor() {
@@ -9,6 +12,11 @@ export default class GuildDelete extends Event {
 
   async run(guild: Guild): Promise<void> {
     if (!this.client.isReady) return;
+
+    await db.transaction(async (tx) => {
+      await tx.delete(news).where(eq(news.guildId, guild.id));
+      await tx.delete(guilds).where(eq(guilds.guildId, guild.id));
+    });
 
     this.client.logger.info(`Leaved ${guild.name} (${guild.id})`);
   }
