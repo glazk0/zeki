b2cbdd21e7a36f4b473f4ce11f5802d0afe1f63a
Théo <43091603+glazk0@users.noreply.github.com>
2023-09-10 12:04:15 +0200

    misc: add error handling

--- src/events/GuildCreate.ts
+++ src/events/GuildCreate.ts
@@ -12,9 +12,13 @@ export default class GuildCreate extends Event {
   async run(guild: Guild): Promise<void> {
     if (!this.client.isReady) return;
 
-    await db.insert(guilds).values({
-      guildId: guild.id,
-    });
+    try {
+      await db.insert(guilds).values({
+        guildId: guild.id,
+      });
+    } catch (error) {
+      this.client.logger.error('Error while inserting a guild', error);
+    }
 
     this.client.logger.info(`Joined ${guild.name} (${guild.id})`);
   }
